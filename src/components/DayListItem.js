0feb11afd1d220ded99cad94efd088ace2ce655d
kimiakalantari <kimiakalantari97@gmail.com>
2023-07-13 13:00:23 -0400

    test daylistitem using unit test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,11 +10,20 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots===0
   })
-  
+
+  const formatSpots = function() {
+    const numOfSpots = props.spots;
+    if(numOfSpots === 0) return "no spots remaining";
+    else if(numOfSpots === 1) return "1 spot remaining";
+    else{
+      return `${numOfSpots} spots remaining`;
+    }
+  }
+
   return (
     <li className ={dayListItemClass} selected={props.selected} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
