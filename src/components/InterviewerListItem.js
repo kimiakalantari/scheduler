d7ae8031ad9c9f86685936d426bcd88630726af7
kimiakalantari <kimiakalantari97@gmail.com>
2023-07-13 19:17:27 -0400

    refactor interviewer components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,20 +6,20 @@ import classnames from 'classnames';
 export default function InterviewerListItem(props) {
 
 
-  const { name, avatar, selected, setInterviewer} = props;
+  const {selected} = props;
 
   const interviewerListItemClass = classnames("interviewers__item",{
     "interviewers__item--selected": selected,
 
   })
 return(
-  <li className={interviewerListItemClass} selected={selected} onClick={setInterviewer}>
+  <li className={interviewerListItemClass} onClick={props.setInterviewer}>
     <img
       className="interviewers__item-image"
-      src={avatar}
-      alt={name}
+      src={props.avatar}
+      alt={props.name}
     />
-    {selected && name}
+    {props.selected && props.name}
   </li>
 );
 
