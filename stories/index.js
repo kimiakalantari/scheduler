5ec14f35b8c2ff2211dcb175fcffc6a6a5abbb32
kimiakalantari <kimiakalantari97@gmail.com>
2023-07-17 17:07:33 -0400

    display appointments

--- stories/index.js
+++ stories/index.js
@@ -162,4 +162,14 @@ storiesOf("InterviewerListItem", module)
       <Appointment id={1} time="12pm" />
       <Appointment id="last" time="1pm" />
     </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
   ));
