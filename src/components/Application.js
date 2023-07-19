b67fd1a08a89af424be78ecc7715577f0221170f
kimiakalantari <kimiakalantari97@gmail.com>
2023-07-18 20:32:18 -0400

    fetch days using API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+
+import axios from 'axios';
 
 import "components/Application.scss";
 
@@ -50,25 +52,17 @@ export default function Application(props) {
 
   //useState hook
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  useEffect(()=>{
+    const dayURL = "http://localhost:8001/api/days";
+    axios.get(dayURL).then(response =>{
+      console.log(response.data)
+      setDays([...response.data]);
+    })
+  },[]);
 
+  
   return (
     <main className="layout">
       <section className="sidebar">
