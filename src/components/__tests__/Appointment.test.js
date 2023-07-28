72321a319809769dd3c9e428f81af410c47fd45f
kimiakalantari <kimiakalantari97@gmail.com>
2023-07-28 19:19:20 -0400

    add our first test for the Appointment component

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { render} from "@testing-library/react";
+
+import Appointment from "components/Appointment/index";
+import Form from "components/Appointment/Form";
+
+
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  it("renders from without crashing", () => {
+    render(<Form interviewers ={[]}/>);
+  });
+});
