809995e7e54ef07ca36960ad479fbebd8b0b10f0
kimiakalantari <kimiakalantari97@gmail.com>
2023-07-28 19:44:21 -0400

    implement the test for the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList"
 export default function Form(props) {
   const [interviewer, setInterviewer]= useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
+  const [error, setError] = useState("");
 
   const reset = function(){
     setName("");
@@ -13,6 +14,22 @@ export default function Form(props) {
   const cancel = function(){
     reset();
     props.onCancel();
+  }
+
+  //to validate that the name input cannot be empty
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    
+    setError("");
+    props.onSave(name, interviewer);
   }
 
   return(
@@ -26,18 +43,18 @@ export default function Form(props) {
           placeholder="Enter Student Name"
           value={name}
           onChange={(event) => setName(event.target.value)}
-
-          /*
-            This must be a controlled component
-          */
+          data-testid="student-name-input"
          />
+
+        <section className="appointment__validation">{error}</section>
+
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
         </section>
         <section className="appointment__card-right">
           <section className="appointment__actions">
             <Button danger onClick={cancel}>Cancel</Button>
-            <Button confirm onClick={()=>props.onSave(name, interviewer)} >Save</Button>
+            <Button confirm onClick={event => validate()} >Save</Button>
           </section>
         </section>
     </main> 
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -3,16 +3,11 @@ import React from "react";
 import { render} from "@testing-library/react";
 
 import Appointment from "components/Appointment/index";
-import Form from "components/Appointment/Form";
 
 
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
-  });
-
-  it("renders from without crashing", () => {
-    render(<Form interviewers ={[]}/>);
   });
 });
